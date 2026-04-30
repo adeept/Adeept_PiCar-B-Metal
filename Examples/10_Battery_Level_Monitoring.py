4209f795fc704017c25c43729b3035c64df35c72
Adeept <support@adeept.com>
2026-04-30 13:39:02 +0800

    Add files via upload

--- Examples/10_Battery_Level_Monitoring.py
+++ Examples/10_Battery_Level_Monitoring.py
@@ -4,40 +4,37 @@
 # Author      : Adeept
 # Date        : 2025/03/11
 import time
-import board
-import busio
-from adafruit_bus_device.i2c_device import I2CDevice
+import smbus
 
-i2c = busio.I2C(board.SCL, board.SDA)
-# ADS7830 adress 0x48
-device = I2CDevice(i2c, 0x48)
 
-# Define constants
-Vref = 8.4
-WarningThreshold = 6.75
+average_voltage = 0.0
+
+full_voltage = 8.4
+WarningThreshold = 6.3
+# read the ADC value of channel 0
+ADCVref = 5.2
+channel = 0
 R15 = 3000
 R17 = 1000
 DivisionRatio = R17 / (R15 + R17)
 
-#Define the ADC channel and command.
-cmd = 0x84
-channel = 0
-control_byte = cmd | (((channel << 2 | channel >> 1) & 0x07) << 4)
+class ADS7830(object):
+    def __init__(self):
+        self.cmd = 0x84
+        self.bus=smbus.SMBus(1)
+        self.address = 0x48 # 0x48 is the default i2c address for ADS7830 Module.   
+        
+    def analogRead(self, chn): # ADS7830 has 8 ADC input pins, chn:0,1,2,3,4,5,6,7
+        value = self.bus.read_byte_data(self.address, self.cmd|(((chn<<2 | chn>>1)&0x07)<<4))
+        return value
+
 
 if __name__ == "__main__":
-    buffer = [1]
+    adc = ADS7830()
     while True:
-        device.write_then_readinto(bytes([control_byte]), buffer)
-        adcValue = buffer[0]
-        print(str(adcValue))
-        A0Voltage = (adcValue / 255) * 5
-        ActualBatteryVoltage = A0Voltage / DivisionRatio
-
-        BatteryPercentage = (ActualBatteryVoltage - WarningThreshold) / (Vref - WarningThreshold) * 100
-
-        print(f"Current battery level: {BatteryPercentage:.2f} %")
-
-        # Battery level warning judgment
-        if BatteryPercentage < 20:
-            print("Warning! The battery level is too low. Please charge in time!")
+        ADCValue = adc.analogRead(channel)    # read the ADC value of channel 0
+        A0Voltage = ADCValue / 255.0 * ADCVref  # calculate the voltage value
+        actual_battery_voltage = A0Voltage / DivisionRatio
+        percentage = int((actual_battery_voltage - WarningThreshold) / (full_voltage - WarningThreshold) * 100)
+        print ('ADC Value : %d, actual_battery_voltage %.2f, percentage: %d%%'%(ADCValue, actual_battery_voltage, percentage))
         time.sleep(0.5)
